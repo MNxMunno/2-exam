d3b9e7db52016f73888eef52ab48304b21dea1d6
MNxMunno <bilolhuzayfa@gmail.com>
2023-09-29 01:00:41 +0500

    change

--- js/main.js
+++ js/main.js
@@ -11,9 +11,3 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
-
-let modeBtn = document.getElementById("dark-light");
-
-modeBtn.addEventListener("click", function () {
-  document.body.classList.toggle("dark");
-});
